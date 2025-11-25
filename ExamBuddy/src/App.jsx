46351404e4d332a764b5a45973dd8368b7f2c611
Aarya Chaudhari <164394872+Aarya-Chaudhari@users.noreply.github.com>
2025-11-25 10:05:56 +0530

    Update App.jsx

--- ExamBuddy/src/App.jsx
+++ ExamBuddy/src/App.jsx
@@ -1,5 +1,10 @@
+import React from "react";
 import Win7DesktopAdvanced from "./Win7Desktop-Advanced.jsx";
 
 export default function App() {
-  return <Win7DesktopAdvanced />;
+  return (
+    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <Win7DesktopAdvanced />
+    </div>
+  );
 }
