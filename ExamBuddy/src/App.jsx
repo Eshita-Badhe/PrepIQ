4c23329613c00f36b063af0df61157e681f50045
Aarya Chaudhari <164394872+Aarya-Chaudhari@users.noreply.github.com>
2025-11-25 10:03:56 +0530

    Update App.jsx

--- ExamBuddy/src/App.jsx
+++ ExamBuddy/src/App.jsx
@@ -1,4 +1,4 @@
-import Win7DesktopAdvanced from "./Win7Desktop-Advanced";
+import Win7DesktopAdvanced from "./Win7Desktop-Advanced.jsx";
 
 export default function App() {
   return <Win7DesktopAdvanced />;
